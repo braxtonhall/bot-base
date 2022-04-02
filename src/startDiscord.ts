8b86a75ee603203477b950220081db019a668ba6
braxtonhall <braxtonjhall@gmail.com>
2022-04-01 21:43:22 -0700

    Expose partials from Discord API

--- src/startDiscord.ts
+++ src/startDiscord.ts
@@ -1,4 +1,4 @@
-import {BitFieldResolvable, Client, IntentsString} from "discord.js";
+import {BitFieldResolvable, Client, IntentsString, PartialTypes} from "discord.js";
 import {registerCommands} from "./commands/Command";
 import {registerListeners} from "./listeners/Listener";
 
@@ -6,12 +6,13 @@ type Options = {
     commandDirectory: string,
     listenerDirectory: string,
     intents: BitFieldResolvable<IntentsString, number>,
-    token: string
+    token: string,
+    partials?: PartialTypes[],
 };
 
 const startDiscord = async (options: Options): Promise<Client> => {
-    const {commandDirectory, listenerDirectory, intents, token} = options;
-    const client = new Client({intents});
+    const {commandDirectory, listenerDirectory, intents, token, partials} = options;
+    const client = new Client({intents, partials});
     const withCommands = await registerCommands(client, commandDirectory);
     const withListeners = await registerListeners(withCommands, listenerDirectory);
     await withListeners.login(token);
