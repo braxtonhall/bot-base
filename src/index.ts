28f374cfaa6286009c11ebdda27f55dd7494c8fd
braxtonhall <braxtonjhall@gmail.com>
2023-04-08 02:05:54 +0800

    Add basic repo hygiene

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
-import {startDiscord} from "./startDiscord";
-import {DatabaseController, getDatabaseController} from "./controllers/database/DatabaseController";
-import {Listener} from "./listeners/Listener";
-import {Command} from "./commands/Command";
+import { startDiscord } from "./startDiscord";
+import {
+	DatabaseController,
+	getDatabaseController,
+} from "./controllers/database/DatabaseController";
+import { Listener } from "./listeners/Listener";
+import { Command } from "./commands/Command";
 import Log from "./util/Log";
 
-export {startDiscord, DatabaseController, getDatabaseController, Listener, Command, Log};
+export {
+	startDiscord,
+	DatabaseController,
+	getDatabaseController,
+	Listener,
+	Command,
+	Log,
+};
